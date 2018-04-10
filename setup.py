3d6b0dfe807371f6dbbd1b9195f139120bb425d2
Nick McCoy <nick@stitchdata.com>
2018-04-10 09:51:50 -0400

    bumping version to 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-github',
-      version='0.4.1',
+      version='0.5.0',
       description='Singer.io tap for extracting data from the GitHub API',
       author='Stitch',
       url='http://singer.io',
