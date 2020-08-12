76b89c407704aacdfb38358d7a88b36359e56e5c
Jacob Baca <jbaca@stitchdata.com>
2020-08-12 20:49:57 +0000

    added sync function for issue_events

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -27,6 +27,7 @@ KEY_PROPERTIES = {
     'review_comments': ['id'],
     'pr_commits': ['id'],
     'events': ['id'],
+    'issue_events': ['id'],
     'issue_labels': ['id'],
     'issue_milestones': ['id'],
     'pull_request_reviews': ['id'],
@@ -289,6 +290,48 @@ def get_all_team_memberships(team_slug, schemas, repo_path, state, mdata):
                     counter.increment()
                     yield rec
     return state
+
+
+def get_all_issue_events(schemas, repo_path, state, mdata):
+    bookmark_value = get_bookmark(state, repo_path, "issue_events", "since")
+    if bookmark_value:
+        bookmark_time = singer.utils.strptime_to_utc(bookmark_value)
+    else:
+        bookmark_time = 0
+
+
+    #missing “issues events” /repos/{owner}/{repo}/issues/events
+    # headers={"Accept": "application/vnd.github.starfox-preview"}
+    with metrics.record_counter('issue_events') as counter:
+        for response in authed_get_all_pages(
+                'issue_events',
+                # change the url
+                # 'https://api.github.com/repos/{}/events?sort=created_at&direction=desc'.format(repo_path)
+                'https://api.github.com/repos/{}/issues/events?sort=created_at&direction=desc'.format(repo_path)
+                #'https://api.github.com/repos/{}/events?sort=created_at&direction=desc'.format(repo_path)
+        ):
+            events = response.json()
+            extraction_time = singer.utils.now()
+            for event in events:
+                event['_sdc_repository'] = repo_path
+
+                # skip records that haven't been updated since the last run
+                # the GitHub API doesn't currently allow a ?since param for pulls
+                # once we find the first piece of old data we can return, thanks to
+                # the sorting
+                updated_at = event.get('created_at') if event.get('updated_at') is None else event.get('updated_at')
+                if bookmark_time and singer.utils.strptime_to_utc(updated_at) < bookmark_time:
+                    return state
+
+                # transform and write release record
+                with singer.Transformer() as transformer:
+                    rec = transformer.transform(event, schemas, metadata=metadata.to_map(mdata))
+                singer.write_record('issue_events', rec, time_extracted=extraction_time)
+                singer.write_bookmark(state, repo_path, 'issue_events', {'since': singer.utils.strftime(extraction_time)})
+                counter.increment()
+
+    return state
+
 
 def get_all_events(schemas, repo_path, state, mdata):
     # Incremental sync off `created_at`
@@ -867,6 +910,7 @@ SYNC_FUNCTIONS = {
     'releases': get_all_releases,
     'stargazers': get_all_stargazers,
     'events': get_all_events,
+    'issue_events': get_all_issue_events,
     'issue_milestones': get_all_issue_milestones,
     'issue_labels': get_all_issue_labels,
     'projects': get_all_projects,
