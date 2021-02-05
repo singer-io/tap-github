1dde7a0abede332ec952814c6478cd6dfcb4f129
Andy Lu <andy@stitchdata.com>
2021-02-05 12:33:13 -0600

    Bump to v1.9.1, update changelog (#106)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-github',
-      version='1.9.0',
+      version='1.9.1',
       description='Singer.io tap for extracting data from the GitHub API',
       author='Stitch',
       url='http://singer.io',
