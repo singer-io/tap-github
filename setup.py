632c9377750dc52a286ce0910f0b0eed139d82a1
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-05-21 15:38:04 +0530

    Incorporated best practices

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='tap-github',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_github'],
       install_requires=[
-          'singer-python==5.3.3',
+          'singer-python==5.12.1',
           'requests==2.20.0'
       ],
       extras_require={
