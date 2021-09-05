b1c1002bdacb9b8a276c1ff6c706aa755101ba41
KBorders01 <kevin@collage.com>
2021-09-05 12:17:53 -0400

    including user types in prs, and stopping rate limit from breaking everything

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -193,9 +193,9 @@ def rate_throttling(response):
     if int(response.headers['X-RateLimit-Remaining']) == 0:
         seconds_to_sleep = calculate_seconds(int(response.headers['X-RateLimit-Reset']))
 
-        if seconds_to_sleep > 600:
-            message = "API rate limit exceeded, please try after {} seconds.".format(seconds_to_sleep)
-            raise RateLimitExceeded(message) from None
+        #if seconds_to_sleep > 600:
+        #    message = "API rate limit exceeded, please try after {} seconds.".format(seconds_to_sleep)
+        #    raise RateLimitExceeded(message) from None
 
         logger.info("API rate limit exceeded. Tap will retry the data collection after %s seconds.", seconds_to_sleep)
         time.sleep(seconds_to_sleep)
