a986179caec7c0f3b215942edb20fda607675ab7
Andrey Kabanov <andrey.kabanov@bytecode.io>
2021-12-07 12:58:30 -0800

    add NotFoundException handling to collaborators stream

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -920,19 +920,27 @@ def get_all_collaborators(schema, repo_path, state, mdata, _start_date):
     https://developer.github.com/v3/repos/collaborators/#list-collaborators
     '''
     with metrics.record_counter('collaborators') as counter:
-        for response in authed_get_all_pages(
-                'collaborators',
-                'https://api.github.com/repos/{}/collaborators'.format(repo_path)
-        ):
-            collaborators = response.json()
-            extraction_time = singer.utils.now()
-            for collaborator in collaborators:
-                collaborator['_sdc_repository'] = repo_path
-                with singer.Transformer() as transformer:
-                    rec = transformer.transform(collaborator, schema, metadata=metadata.to_map(mdata))
-                singer.write_record('collaborators', rec, time_extracted=extraction_time)
-                singer.write_bookmark(state, repo_path, 'collaborators', {'since': singer.utils.strftime(extraction_time)})
-                counter.increment()
+        try:
+            responses = authed_get_all_pages(
+                    'collaborators',
+                    'https://api.github.com/repos/{}/collaborators'.format(repo_path)
+                )
+        except NotFoundException as error:
+            logger.info(
+                'Unable to retreive collaborators stream, check access_token is valid for {}. See full error message: {}'.format(
+                    repo_path, error)
+                )
+        else:
+            for response in responses:
+                collaborators = response.json()
+                extraction_time = singer.utils.now()
+                for collaborator in collaborators:
+                    collaborator['_sdc_repository'] = repo_path
+                    with singer.Transformer() as transformer:
+                        rec = transformer.transform(collaborator, schema, metadata=metadata.to_map(mdata))
+                    singer.write_record('collaborators', rec, time_extracted=extraction_time)
+                    singer.write_bookmark(state, repo_path, 'collaborators', {'since': singer.utils.strftime(extraction_time)})
+                    counter.increment()
 
     return state
 
