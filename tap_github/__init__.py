1ab34880eeae7b04f86aac55ca8ae72feb9cc360
Raul Gonzalez Martinez <raul.gonzalez@smartprotection.com>
2022-01-04 16:22:28 +0100

    feat(pulls): Enhance PR Reviews data
    Adding PR Number. We want to be able to join rows between PR Table and their reviews

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -864,11 +864,12 @@ def get_pr_detail(pull_number, repo_path):
 def get_reviews_for_pr(pr_number, schema, repo_path, state, mdata):
     for response in authed_get_all_pages(
             'reviews',
-            'https://api.github.com/repos/{}/pulls/{}/reviews'.format(repo_path,pr_number)
+            'https://api.github.com/repos/{}/pulls/{}/reviews'.format(repo_path, pr_number)
     ):
         reviews = response.json()
         for review in reviews:
             review['_sdc_repository'] = repo_path
+            review['pull_request_number'] = pr_number
             with singer.Transformer() as transformer:
                 rec = transformer.transform(review, schema, metadata=metadata.to_map(mdata))
             yield rec
