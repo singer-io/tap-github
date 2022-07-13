4fd734ba32fb7985568a00d82928b8d615548986
prijendev <prijen.khokhani@crestdatasys.com>
2022-07-13 10:06:06 +0530

    Added unittest cases.

--- tests/unittests/test_get_streams_and_state_translate.py
+++ tests/unittests/test_get_streams_and_state_translate.py
@@ -2,6 +2,7 @@ import unittest
 from tap_github.sync import get_selected_streams, translate_state, get_stream_to_sync
 
 def get_stream_catalog(stream_name, selected_in_schema = False, selected_in_metadata = False):
+    """Return catalog for stream"""
     return {
                 "schema":{"selected": selected_in_schema},
                 "tap_stream_id": stream_name,
@@ -14,8 +15,12 @@ def get_stream_catalog(stream_name, selected_in_schema = False, selected_in_meta
             }
 
 class TestGetSelectedStreams(unittest.TestCase):
+    """
+    Testcase for `get_selected_streams` in sync
+    """
 
     def test_selected_in_schema(self):
+        """Verify if stream is selected in schema"""
         catalog = {
             "streams": [
                 get_stream_catalog("assignees", selected_in_schema=True),
@@ -25,6 +30,7 @@ class TestGetSelectedStreams(unittest.TestCase):
         self.assertListEqual(["assignees"],get_selected_streams(catalog))
 
     def test_selected_in_metadata(self):
+        """Verify if stream is selected in metadata"""
         catalog = {
             "streams": [
                 get_stream_catalog("assignees", selected_in_metadata=True),
@@ -36,6 +42,9 @@ class TestGetSelectedStreams(unittest.TestCase):
         self.assertListEqual(["assignees", "commits"], get_selected_streams(catalog))
 
 class TestTranslateState(unittest.TestCase):
+    """
+    Testcase for `translate_state` in sync
+    """
 
     catalog = {
         "streams": [
@@ -47,6 +56,7 @@ class TestTranslateState(unittest.TestCase):
     }
 
     def test_state_with_repo_name(self):
+        """Verify for state with repo name"""
         state = {
             "bookmarks": {
                 "org/test-repo" : {
@@ -60,6 +70,7 @@ class TestTranslateState(unittest.TestCase):
         self.assertEqual(state, dict(final_state))
 
     def test_state_without_repo_name(self):
+        """Verify for state without repo name"""
         state = {
             "bookmarks": {
                 "comments": {"since": "2019-01-01T00:00:00Z"}
@@ -79,13 +90,17 @@ class TestTranslateState(unittest.TestCase):
         self.assertEqual(expected_state, dict(final_state))
 
     def test_with_empty_state(self):
+        """Verify for empty state"""
 
         final_state = translate_state({}, self.catalog, ["org/test-repo"])
 
         self.assertEqual({}, dict(final_state))
 
 class TestGetStreamsToSync(unittest.TestCase):
-    
+    """
+    Testcase for `get_stream_to_sync` in sync
+    """
+
     def get_catalog(self, parent=False, mid_child = False, child = False):
         return {
             "streams": [
@@ -100,6 +115,7 @@ class TestGetStreamsToSync(unittest.TestCase):
         }
 
     def test_parent_selected(self):
+        """Test if only parents selected"""
         expected_streams = ["assignees", "projects", "teams"]
         catalog = self.get_catalog(parent=True)
         sync_streams = get_stream_to_sync(catalog)
@@ -107,6 +123,7 @@ class TestGetStreamsToSync(unittest.TestCase):
         self.assertEqual(sync_streams, expected_streams)
     
     def test_mid_child_selected(self):
+        """Test if only mid child selected"""
         expected_streams = ["projects", "project_columns", "teams", "team_members"]
         catalog = self.get_catalog(mid_child=True)
         sync_streams = get_stream_to_sync(catalog)
@@ -114,6 +131,7 @@ class TestGetStreamsToSync(unittest.TestCase):
         self.assertEqual(sync_streams, expected_streams)
     
     def test_lowest_child_selected(self):
+        """Test if only lower child selected"""
         expected_streams = ["projects", "project_columns", "project_cards", 
                             "teams", "team_members", "team_memberships"]
         catalog = self.get_catalog(child=True)
