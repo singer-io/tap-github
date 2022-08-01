9fbe5791cde17db079b519da61e24bfa8a1e6eed
NevilParikh14 <nevil.parikh@crestdatasys.com>
2022-08-01 14:33:55 +0530

    resolved review comment

--- tests/test_github_start_date.py
+++ tests/test_github_start_date.py
@@ -151,6 +151,9 @@ class GithubStartDateTest(TestGithubBase):
                 primary_keys_sync_1 = set(primary_keys_list_1)
                 primary_keys_sync_2 = set(primary_keys_list_2)
 
+                # verify that sync 2 has at least one record synced
+                self.assertGreater(record_count_sync_2, 0)
+
                 if expected_metadata.get(self.OBEYS_START_DATE):
                     
                     # Expected bookmark key is one element in set so directly access it
